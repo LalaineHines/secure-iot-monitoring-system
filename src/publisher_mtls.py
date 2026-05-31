c649f614a6c19750fc8f48e9561d9964a455ec7c
Lalaine Hines <150487195+LalaineHines@users.noreply.github.com>
2026-05-31 13:29:29 -0700

    Implement MQTT client for publishing sensor data

--- src/publisher_mtls.py
+++ src/publisher_mtls.py
@@ -1,1 +1,51 @@
+import json
+import ssl
+import time
+import paho.mqtt.client as mqtt
 
+from sensor_data import (
+    generate_pool_data,
+    generate_kitchen_data,
+    generate_spa_data
+)
+
+from config import *
+
+client = mqtt.Client(
+  client_id="sensor_publisher",
+  protocol=mqtt.MQTTv311
+)
+
+client.tls_set(
+  ca_certs=CA_CERT,
+  certfile=CLIENT_CERT,
+  keyfile=CLIENT_KEY,
+  tls_version=ssl.PROTOCOL_TLS_CLIENT
+)
+
+client.connect(BROKER_HOST, BROKER_PORT)
+
+while True:
+
+  pool = generate_pool_data()
+  kitchen = generate_kitchen_data()
+  spa = generate_spa_data()
+
+  client.publish(
+    "hydroficient/grandmarina/pool",
+    json.dump(pool)
+  )
+
+  client.publish(
+    "hydroficient/grandmarina/kitchen",
+    json.dump(kitchen)
+  )
+
+  client.publish(
+    "hydroficient/grandmarina/spa",
+    json.dump(spa)
+  )
+
+  print("Published sensor readings")
+
+  time.sleep(5)
