ac79a1908d1a36158f45f800bc6c8be695204ca8
Lalaine Hines <150487195+LalaineHines@users.noreply.github.com>
2026-05-31 13:05:27 -0700

    Add configuration for broker and topics

---
+++ src/config.py
@@ -0,0 +1,12 @@
+BROKER_HOST = "localhost"
+BROKER_PORT = 8883
+
+TOPICS = [
+  "hydroficient/grandmarina/pool",
+  "hydroficient/grandmarina/kitchen",
+  "hydroficient/grandmarina/spa"
+]
+
+CA_CERT = "../certs/ca.crt"
+CLIENT_CERT = "../certs/client.crt"
+CLIENT_KEY = "../certs/client.key"
